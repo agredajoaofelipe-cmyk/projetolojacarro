908a301490ac7f45f6df9ea37ca8444e62264729
joao <agredajoaofelipe@gmail.com>
2026-04-23 15:46:24 -0300

    c

---
+++ script/scripts.js
@@ -0,0 +1,8 @@
+function SalvarCarros(event) {
+    //impedir de carregar a página
+    event.preventDefault();
+    let titulo = document.getElementById('title');
+    let preco = document.getElementById('preco');
+    let marca = document.getElementById('marca');
+    let modelo = document.getElementById('modelo');
+}
